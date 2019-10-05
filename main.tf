82733bc76b0902d6f76642396899c43ec76b122c
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-05 06:14:02 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+
+ subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       =  "${var.client_id}"
+  client_secret   =  "${var.client_secret}"
+ 
+  
+  }
+
+resource "azurerm_resource_group" "pwc-test" {
+
+ name     = "pwc-test"
+
+ location = "eastus"
+
+}
+
+ 
+
+resource "azurerm_snapshot" "pwc-test" {
+
+  name                = "pwc-snapshot_name3"
+
+  location            = "eastus"
+
+  resource_group_name = "pwc-test"
+
+  create_option       = "Copy"
+
+  source_uri          = "/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/PWC-TEST/providers/Microsoft.Compute/disks/vm123_OsDisk_1_679b9c00ae0741d4a5be47c2ae0be5b7"
+
+}
+
+
